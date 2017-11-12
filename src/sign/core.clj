aa124bf4887de75c8fb9db5fd1eb21d69049160a
claus <claus.z@protonmail.com>
2017-11-12 20:02:23 +1100

    Initial comit

---
+++ src/sign/core.clj
@@ -0,0 +1,33 @@
+(ns sign.core
+  (:gen-class))
+
+(defn print-border
+  " Prints a followed by l times b and finishes with another a. "
+  [a b l]
+  (println (apply str a (apply str (repeat l b)) a)))
+
+(defn print-top-bottom
+  [l]
+  (print-border "+" "-" l))
+
+(defn print-side-border
+  [l]
+  (print-border "|" " " l))
+
+(defn print-text
+  [text]
+  (println (apply str "| " text " |")))
+
+(defn print-sign
+  [text]
+  (let [length (+ 2 (count text))]
+    (print-top-bottom length)
+    (print-side-border length)
+    (print-text text)
+    (print-side-border length)
+    (print-top-bottom length)))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (print-sign (first args)))
---
+++ test/sign/core_test.clj
@@ -0,0 +1,7 @@
+(ns sign.core-test
+  (:require [clojure.test :refer :all]
+            [sign.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
