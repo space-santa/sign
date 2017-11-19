b1b112d9327eb2555579c0168a5718b6f6154889
claus <claus.z@protonmail.com>
2017-11-19 22:08:12 +1100

    Remove some things to basically start the whole thing from semi-scratch

--- src/sign/core.clj
+++ src/sign/core.clj
@@ -1,7 +1,7 @@
 (ns sign.core
   (:gen-class))
 
-(def max-char 40)
+(def max-chars 40)
 
 (defn move-token
   [a b]
@@ -11,11 +11,9 @@
         new-b (clojure.string/join " " rest)]
     [new-a new-b]))
 
-(defn make-max-strings
-  [text l]
-  (let [tokens (clojure.string/split text #"\s")
-        [first & rest] tokens
-        text (clojure.string/join " "  [text first])]))
+(defn tokenize-string
+  [text]
+  (clojure.string/split text #"\s"))
 
 (defn print-border
   " Prints a followed by l times b and finishes with another a. "
@@ -34,35 +32,12 @@
   [text]
   (println (apply str "|  " text "  |")))
 
-(defn max-length
-  [text]
-  (+ 4 (count text)))
-
-(defn make-lines
-  "Takes a seq of words and concats them together.
-    Returns a seq of lines, each of them shorter than limit."
-  [words limit])
-
-(defn split-at-limit
-  [text limit]
-  (if (< limit (count text))
-    (let [pos (clojure.string/last-index-of text " " 40)
-          a (subs text 0 pos)
-          b (subs text (+ 1 pos) (count text))]
-      [a b])
-    [text]))
-
-(defn make-sign-lines
-  [text limit]
-  (loop [t text]
-    (let [tmp (split-at-limit t limit)]
-      (println tmp)
-      (if (< limit (count (tmp 1)))
-        (recur (tmp 1))))))
+(defn longest-line
+  [lines])
 
 (defn print-sign
   [text]
-  (let [length (max-length text)]
+  (let [length (longest-line text)]
     (print-top-bottom length)
     (print-side-border length)
     (print-text text)
