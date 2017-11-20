8d85b1f9ed24ef07ebe48d4cd6df3d7e37792c35
claus <claus.z@protonmail.com>
2017-11-21 08:45:09 +1100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject sign "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject sign "1.0.0"
+  :description "Makes a string of all args and prints it in a frame."
+  :url "https://github.com/space-santa/sign"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
